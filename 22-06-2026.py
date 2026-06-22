c3f3b352b60fe719a033669e51abb4136e90ca66
Anu5156 <anukuacharya@gmail.com>
2026-06-22 20:56:32 +0530

    22-06-2026

--- 22-06-2026.py
+++ 22-06-2026.py
@@ -1,5 +1,6 @@
 # what is data structure ?
 # the data structure is a particular way of organizing data in  a computers MEMORY so that it can be used effectively by program code.
+#data structure=organized data
 
 # what is an algorithm?
 # algorithm is a step-by-step procedure used to solve a problem or perform a computation
@@ -10,13 +11,56 @@
 #what is software?
 # software is a collection of instructions that tells a computer what to do.
 
-a=int(input("enter the number:"))
-b=int(input("enter the number:"))
-print(a)
-print(a+b)
+# a=int(input("enter the number:"))
+# b=int(input("enter the number:"))
+# print(a)
+# print(a+b)
 
-print("hello world")#hello world
+# print("hello world")#hello world
 
 #data can be organized or unorganized way
 #organized data can be searched easily
 #unorganized data are difficult to search
+
+# features of the algorithm:
+#1.range of input
+#2.non ambiguity
+#3.the same algorithm can be represented in different ways
+#4.several algorithm for solving the same problem
+#5.different algorithm will run with different speed.
+
+# what is pseudo(fake) code?
+#it cannot be run directly on the computer
+#to form the psuedo code no programming language is required
+#they are plan/human language
+#they are human readable language
+#they are non executable
+"""
+ex:to add the 2 number
+step1:take first number in 'a'
+step2:take second number in 'b'
+step3:perform arithmatic operation(a+b)
+step4:print the obtained output
+
+
+
+ex2:to find the maximum of the 2 number
+step1:take the 2 number in a & b
+step2:compare a and b ..if a is greater than b, then a is maximum number,if b is greater than a then b is maximum number
+
+"""
+
+#source code:they are executable ..python interpreter will execute
+"""# a=int(input("enter the number:"))
+# b=int(input("enter the number:"))
+# print(a)
+# print(a+b)"""
+
+"""a=10
+b=40
+print(max(a,b))#40"""
+
+#flowchart:it is the pictoral representation of the algorithm
+"""the AMERICAN NATIONAL STANDARDS INSTITUTE set standards for flowcharts and their symbols in the 1960s
+"""
+
