54f922285f27cef18a4eae43c0264bddf1852a12
Anu5156 <anukuacharya@gmail.com>
2026-06-24 09:48:26 +0530

    24-06-26

--- 23-06-2026.py
+++ 23-06-2026.py
@@ -9,5 +9,56 @@ UML can be for modelling a system independent of the a language
 UML gives a stay many to write a system model,covering ideas
 
 """
+#types of UML:
+"""
+1.Structural
+2.Behavioural
+3.interaction
 
+"""
+#structural diagram
+#they are used to represent a static view of the system.it represents  a part of the system that makes up the structure of the system
+# 1.class diagram
+# 2.component diagram
+# 3.node diagram
+# 4.package diagram
+# 5.profile diagram
+# 6.object diagram
+# 7.deployment diagram
 
+#behavioural
+#a real world system can be represented in either a static form or dynamic form 
+#structural diagram shows the static form where as behavioural diagram shows the dynamic form
+"""
+types of behavioural diagram in UMl
+1.activity diagram:it explain better than flow chart
+2.state machine diagram
+3.use case diagram
+4.sequence diagram
+5.communication diagram
+6.timing diagram
+
+"""
+"""
+#interaction diagram
+it is the subset of the behavioural diagram.it is udes to flow b/w various use case elements of a system.interaction diagrams are used to show the interaction b/w 2 entities and how data flows within them
+"""
+# Types of interaction diagrams
+
+"""
+1.use case diagram: it is the main diagram of the interaction diagram.it is used to show the interaction b/w 2 entities and how data flows within them
+2.sequence diagram:it is used to show the interaction b/w 2 entities and how data flows within them
+3.communication diagram:it is used to show the interaction b/w 2 entities and how data flows within them
+4.timing diagram:it is used to show the interaction b/w 2 entities and how data flows within them
+
+"""
+#time complexity: 
+"""
+time complexity is the amount of time it takes for an algorithm to run as a function of the input size
+
+"""
+#space complexity:
+
+"""
+amount of  memory an algorithmuses as the input size 'n'grows.
+"""
