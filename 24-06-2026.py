b13bd329baeed97e0a98bf3f4100bb1fc6929a28
Anu5156 <anukuacharya@gmail.com>
2026-06-24 15:41:46 +0530

    24-06-26

---
+++ 24-06-2026.py
@@ -0,0 +1,75 @@
+#data:
+"""
+the raw values/facts are called as data.it can be anything like numbers,text,symbols,etc
+"""
+
+#datastructure:the logical/mathematical model particular organization of data is called Data structure.
+#database:it is an organized collection of data that is structured in a tabular format.
+#Database management system:it is a software that is used to manage and maintain the database.
+
+
+#array:
+#array is a data structure that is used to store a collection of elements of the same data type
+#array is a linear data structure
+
+#what are the difference between datastructure and database
+
+"""
+datastructure:it organizes the data in ram
+
+data base:it organises the data in storage"""
+
+#classification of data structure:
+#1.PRIMITIVE DATA STRUCTURE
+#2.NON PRIMITIVE DATA STRUCTURE
+
+"""
+PRIMITIVE DATA STRUCTURE
+these are the basic data structure that are used to store the data in a computer memory
+
+example:
+1.integer
+2.float
+3.character
+4.boolean
+5.complex
+6.double
+7.string
+8.pointer
+9.enum
+
+NON PRIMITIVE DATA STRUCTURE
+these are the complex data structure that are used to store the data in a computer memory
+
+example:
+1.array
+2.linked list
+3.stack
+4.queue
+5.tree
+6.graph
+7.map/associated array/hash table/dictionary
+8.set
+9.vector
+10.multimap
+11.multiset
+12.bag
+13.file
+14.set
+15.tuple
+16.string
+17.struct
+18.union
+"""
+"""
+properties of Data structure
+
+1.non primitive data structure
+--->when there are
+     a.multiple data
+       --->linear data and non-linear data
+     b.different data types
+       --->homogeneous and heterogeneous
+"""
+    
+
